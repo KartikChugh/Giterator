ffc03e6db4bc5de90f24dcd8079baa59de71f9aa
KartikChugh <kartikchugh.kc@gmail.com>
2020-05-06 13:40:31 -0400

    Rewrites chunking process
    Fixes #1

--- giterate.sh
+++ giterate.sh
@@ -1,57 +1,65 @@
 #!/bin/bash
 
-# params - source file, dest folder, hrs
+# Parameters
 FILE=$1
 DEST=$2
 HRS=$3
+CHUNKS=$4
+
+if [ ! -d "$DEST" ]
+then
+    echo "giterator >> Destination $DEST doesn't exist"
+    exit 1
+fi
 
 cd "$DEST"
 
-# check if repo exists at dest, if not, make it
+# Make repo at destination if non-existent
 if [ -d ".git/" ]
 then
-    echo "Repo exists at directory"
+    echo "giterator >> Repo exists at directory"
 else
-    echo "Repo does not exist"
+    echo "giterator >> Repo does not exist"
     git init
 fi
 
-# divide dest into 5 line chunks, store in array e.g. [0..,5..,10..,15..,21] 
-lines=`wc -l < "$FILE"`
-chunk_size=$((lines / 4))
-
-echo "Linecount: $lines"
-echo "Chunk size: $chunk_size lines"
-
-for chunk_i in {0..4}
-do
-    chunks[$chunk_i]=""
-done
-
-i=0
-while IFS= read -r line; do
-    chunk_i=$((i / chunk_size))
-    temp=${chunks[$chunk_i]}
-    temp+=$line
-    temp+=$'\n\r'
-    chunks[$chunk_i]=$temp
-
-    i=$((i+1))
-done < "$FILE"
-
-# loop 5 times: write from chars [i] to [i+1] & commit
+# Store lines of source file into array
+linecount=`wc -l < "$FILE"`
+chunk_size=$((linecount / 4))
 filename="$(basename -- "$FILE")"
-period=$((HRS * 60 * 60 / 5))
+period=$((HRS * 60 * 60 / 5)) # in seconds
 
+echo "giterator >> File name: $filename"
+echo "giterator >> File size: $linecount lines"
+echo "giterator >> Chunk size: $chunk_size lines"
+echo "giterator >> Chunk period: $period seconds"
+
+mapfile -t lines < "$FILE"
+
+# Write to destination file periodically
 for chunk_i in {0..4}
 do
+    line_start=$((chunk_i * chunk_size))
+    line_end=$((line_start + chunk_size-1))
+
+    if (( $line_end > $linecount ))
+    then line_end=$linecount
+    fi
+
+    echo "giterator >> Working on chunk $chunk_i ($line_start to $line_end)"
     sleep $period
-    chunk=${chunks[$chunk_i]}
-    echo $chunk >> "$DEST/$filename"
-    echo "Wrote chunk $chunk_i"
+
+    for (( line_j=$line_start; line_j<=$line_end; line_j++ ))
+    do
+        line=${lines[line_j]}
+        echo $line >> "$filename"
+    done
+
+    echo "giterator >> Wrote chunk $chunk_i"
 
     git add .
     git commit -m "commit $((chunk_i + 1))"
 done
 
+truncate -s -1 "$filename" # Removes ending newline
 echo "Done."
